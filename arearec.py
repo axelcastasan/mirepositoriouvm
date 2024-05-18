9bed34db068097e14adbd938f4c8c2d78dcc9848
Axel Castañeda Sánchez <108374091+axelcastasan@users.noreply.github.com>
2024-05-18 13:52:57 -0600

    Add files via upload

---
+++ arearec.py
@@ -0,0 +1,10 @@
+def area_rectangulo(base, altura):
+    area = base * altura
+    return area
+
+base = 5
+altura = 10
+
+area = area_rectangulo(base, altura)
+
+print("El área del rectangulo es:", area)
